698af9a76d53a0b21fd8bf89719f0a12f37e3e27
Chetan Sharma <csharma1@hawk.iit.edu>
2018-09-21 21:00:53 -0500

    vagrantfile
    provisioning and managing ubuntu instance

---
+++ Vagrantfile.rb
@@ -0,0 +1,7 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/xenial64"
+   config.vm.provision "shell", inline: <<-SHELL
+     apt-get update
+     apt-get install -y apache2
+   SHELL
+end
